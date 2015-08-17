3a551a484abe745291234be79e8138ecca1d8a33
shibi-raj <shibi.1729@gmail.com>
2015-08-17 16:52:38 +0300

    Clean up.

--- clean/bmi_fetch.py
+++ clean/bmi_fetch.py
@@ -1,24 +1,17 @@
 import solr
-
 import time
 
 start_time = time.time()
-
-
-s             = solr.SolrConnection('http://85.31.219.96:7183/solr')
+s          = solr.SolrConnection('http://85.31.219.96:7183/solr')
 
 
 def fetch_all_records():
 
     response      = s.query("id_chart:*", rows="10000", start='0')
-
     id_and_titles = "all_bmi_records_take2.txt"
-
     num_fetched   = 0
     f             = open(id_and_titles, 'w')
 
-    #f.write( str(response.results).encode('utf8') + '\n\n' )
-    
     while(len(response.results) > 0):
         num_fetched += 10000
         print num_fetched
@@ -60,32 +53,12 @@ def fetch_all_record_ids():
 
 def fetch_one():
     response      = s.query("id_chart:*",rows="1",start="399990")
-    for hit in response:
-        print hit
+
 
 def faceted_retrieval():
     
-#    response = s.query("id_chart:*", facet='true', facet_field='best_desmetier', fields='best_desmetier',rows=1000)
     response = s.query("id_chart:*" , fields='best_desmetier, best_segment',rows=1000, facet='true', facet_limit='-1', \
                         ** {'facet.field' : ['best_desmetier']})
-
     return response.results
-
-
-#________________________________
-
-
-#fetch_one()
-fetch_all_records()
-
-
-#for each in faceted_retrieval(): 
-#    print each
-#    for key in each:
-#        print key
-#    for key in list.iterkeys:
-#        print key
-
-
 
 
